6da4f213f8b310d8b84c690bdde3e2923af7ab57
Jakub Mordalski <jakub.mordalski.external@atos.net>
2021-10-27 13:34:14 +0200

    Categories,Products

--- script.js
+++ script.js
@@ -71,3 +71,111 @@ filtersDropdown.addEventListener("click",()=>{
     }
     
 })
+
+
+let categoriesItems = document.getElementById("categories-items");
+
+
+
+let productId
+let output="";
+let chosenCategory=0
+
+
+const generateProducts=()=> {
+    output=""
+    if(chosenCategory==0){
+        fetch("http://localhost:3000/products")
+        .then(res=> res.json())
+        .then(data=> {
+            data.forEach(item=>{
+                output+=`
+                <div class="col-12 col-md-6  col-lg-4">
+                    <div class="card w-100" >
+                        <img src="assets\\img\\7a67c57a311acdbd258694542cf442d6.jpg" class="card-img-top" alt="...">
+                        <img src="svg\\heart.svg" height="25px" class="card-heart" alt="">
+                        <div class="card-body">
+                        <h5 class="card-title">${item.name}</h5>
+                        <p class="card-text text-truncate">${item.description}</p>
+                        <p class="fs-bigger">${item.price} PLN <img src="svg\\add-to-cart.svg" class="card-cart" alt=""></p>
+                        
+                        </div>
+                    </div>
+                </div>
+                `
+            })
+            categoriesItems.innerHTML=output
+        })
+    }else{
+
+    }
+    
+}
+
+generateProducts()
+
+async function generateCategories(){
+    let categoriesCount=0
+    categoriesDropdownBody.innerHTML=""
+    await fetch("http://localhost:3000/categories")
+    .then(res=> res.json())
+    .then(data=>{
+        data.forEach(async function(item){
+            categoriesCount = await getCategoriesCount(item.id)
+            console.log(categoriesCount)
+            categoriesDropdownBody.innerHTML+=`
+            <div id="choose-category-${item.id}" class="category-element container-fluid row m-0 border py-2 rounded-top">
+                <div class="col-10  col-lg-8 p-0 text-truncate">${item.name}</div>
+                <div class="col-2  col-lg-4 p-0">
+                    <p class="categories-count rounded-pill p-0 fs-small text-center text-white m-0 font-weight-bold">${categoriesCount}</p>
+                </div> 
+            </div>
+            `
+            console.log("done")
+        })
+        
+    })
+    
+}
+
+generateCategories()
+
+//  async function getCategoriesCount(){
+//      let promises=[]
+//     await fetch("http://localhost:3000/categories")
+//      .then(res=>res.json())
+//      .then(data=>data.forEach(item=>{
+//          promises.push(fetch(`http://localhost:3000/products/${item.id}`))
+//      }))
+//      Promise.all(promises)
+//      .then(values=>{
+//          let data = []
+//          values.forEach(item=>{
+//              data.push(item.json())
+//          })
+//          return data
+//      })
+//      .then(data=>console.log(data))
+//  }
+
+//  getCategoriesCount()
+
+
+
+ async function getCategoriesCount(id){
+    const categoriesCount = await fetch(`http://localhost:3000/products?category=${id}`)
+    .then(res=> res.json())
+    .then(data=>{return data.length})
+    .catch(error=>{return error})
+    return categoriesCount
+
+}
+
+let categoriesCount
+
+(async ()=>{
+    categoriesCount = await getCategoriesCount(1)
+    
+})()
+
+setTimeout(()=>console.log(categoriesCount),1000)
